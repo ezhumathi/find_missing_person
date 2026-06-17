357064d71b11c86b157f1c5e125513f0b3d912ed
ezhumathi <ezhumathi.s2023ece@sece.ac.in>
2026-06-17 23:08:40 +0530

    third

--- findy/people/models.py
+++ findy/people/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class MissingPerson(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.IntegerField()
+    missing_date=models.DateField()
+    last_seen_Location=models.CharField(max_length=300)
+    contact_info=models.CharField(max_length=200)
+    photo=models.ImageField(upload_to='missing_photos/',blank=True,null=True)
+    description=models.TextField(blank=True,null=True)
+    aadhar_number=models.CharField(max_length=12,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+
 # Create your models here.
