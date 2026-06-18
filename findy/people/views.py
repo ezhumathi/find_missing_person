178caf9507513a611acc446bc21210421e560778
ezhumathi <ezhumathi.s2023ece@sece.ac.in>
2026-06-18 15:21:12 +0530

    four

--- findy/people/views.py
+++ findy/people/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import MissingPerson
 
-# Create your views here.
+
+def search_people(request):
+	q = request.GET.get('q', '').strip()
+	results = []
+	if q:
+		results = MissingPerson.objects.filter(
+			Q(name__icontains=q)
+			| Q(last_seen_Location__icontains=q)
+			| Q(description__icontains=q)
+			| Q(aadhar_number__icontains=q)
+		)
+	return render(request, 'people/search.html', {'query': q, 'results': results})
