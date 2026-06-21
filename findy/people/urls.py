3e3b5eeeb98bceebfcdca74d8d83d0bd2c4839b3
ezhumathi <ezhumathi.s2023ece@sece.ac.in>
2026-06-21 23:58:55 +0530

    today

--- findy/people/urls.py
+++ findy/people/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search_people, name='search_people'),
-    path('/', views.search_people, name='search_people'),
+    path('face-search/', views.face_search, name='face_search'),
 ]
--- findy/people/views.py
+++ findy/people/views.py
@@ -1,16 +1,30 @@
+import face_recognition
 from django.shortcuts import render
-from django.db.models import Q
 from .models import MissingPerson
 
-
-def search_people(request):
-	q = request.GET.get('q', '').strip()
-	results = []
-	if q:
-		results = MissingPerson.objects.filter(
-			Q(name__icontains=q)
-			| Q(last_seen_Location__icontains=q)
-			| Q(description__icontains=q)
-			| Q(aadhar_number__icontains=q)
-		)
-	return render(request, 'people/search.html', {'query': q, 'results': results})
+def face_search(request):
+    results = []
+    message = ''
+    if request.method == 'POST' and request.FILES.get('photo'):
+        upload = request.FILES['photo']
+        image = face_recognition.load_image_file(upload)
+        encodings = face_recognition.face_encodings(image)
+        if not encodings:
+            message = 'No face found in uploaded image.'
+        else:
+            query_encoding = encodings[0]
+            people = MissingPerson.objects.exclude(photo='')
+            for person in people:
+                try:
+                    known_image = face_recognition.load_image_file(person.photo.path)
+                    known_encodings = face_recognition.face_encodings(known_image)
+                    if known_encodings:
+                        match = face_recognition.compare_faces([known_encodings[0]], query_encoding, tolerance=0.5)
+                        if match[0]:
+                            results.append(person)
+                except Exception:
+                    pass
+    return render(request, 'people/face_search.html', {
+        'results': results,
+        'message': message,
+    })
